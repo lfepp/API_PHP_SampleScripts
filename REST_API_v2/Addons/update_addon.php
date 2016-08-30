95b0736cf03bf47f9738557001c49f0e8efb3a03
Luke Epp <lucasfepp@gmail.com>
2016-08-30 14:00:52 -0700

    Added escalation policy examples

--- REST_API_v2/Addons/update_addon.php
+++ REST_API_v2/Addons/update_addon.php
@@ -15,12 +15,12 @@ $JSON = json_encode($PAYLOAD);
 $URL = 'https://api.pagerduty.com/addons/' . urlencode($ADDON_ID);
 $session = curl_init();
 
-curl_setopt($session, CURLOPT_CUSTOMREQUEST, "PUT");
 curl_setopt($session, CURLOPT_URL, $URL);
 curl_setopt($session, CURLOPT_HTTPHEADER, array(
     'Content-type: application/json',
     'Authorization: Token token=' . $API_ACCESS_KEY
 ));
 curl_setopt($session, CURLOPT_POSTFIELDS, $JSON);
+curl_setopt($session, CURLOPT_CUSTOMREQUEST, "PUT");
 $output = curl_exec($session);
 ?>
